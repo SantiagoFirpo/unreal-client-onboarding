2394f92fa6fc7fa2913259bc69fc21e5481e7d6b
Santiago Firpo <santiago.firpo@aquiris.com.br>
2022-04-20 14:39:30 -0300

    Added Intermediate folder to .gitignore

--- Intermediate/Build/Win64/YourFirstHourwithUMGEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.16.27045
-#include "C:/Users/santiago.firpo/Documents/Unreal Projects/YourFirstHourwithUMG/Intermediate/Build/Win64/YourFirstHourwithUMGEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
