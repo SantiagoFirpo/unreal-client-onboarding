1a39b35dc1e0b2efdc0ec9c65db52c61c528e1b5
Santiago Firpo <santiago.firpo@aquiris.com.br>
2022-04-20 14:37:36 -0300

    Added INonPersistent interface for GameInstanceSubsystems

---
+++ Source/YourFirstHourwithUMG/Public/Utilities/UNonPersistent.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "UNonPersistent.generated.h"
+
+UINTERFACE()
+class UNonPersistent : public UInterface
+{
+    GENERATED_BODY()
+};
+
+class YOURFIRSTHOURWITHUMG_API INonPersistent
+{
+    GENERATED_BODY()
+public:
+    virtual void InitializeData() = 0; 
+};
